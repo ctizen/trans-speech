7290d1a946850a803ff20380fea3b94ccc19605a
Oleg Klimenko <me@ctizen.dev>
2023-01-03 00:49:16 +0300

    Initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import MainpageImage from '../../trans-speech/src/img/mainpage.png';
+import NstuImage from '../../trans-speech/src/img/nstu.png';
+import GisImage from '../../trans-speech/src/img/2gis.png';
+import WrikeImage from '../../trans-speech/src/img/wrike.png';
+import VkImage from '../../trans-speech/src/img/vk.png';
+import FpjsImage from '../../trans-speech/src/img/fingerprint.png';
+import customTheme from '../../trans-speech/src/theme/theme-default';
+import { Deck, FlexBox, Heading, Notes, Slide } from "spectacle";
+
+// Reference: https://formidable.com/open-source/spectacle/docs/props
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Deck theme={customTheme}>
+      <Slide>
+        <FlexBox height="100%">
+          <FlexBox height="100%" flexDirection="column">
+            <Heading>Трудности перевода</Heading>
+            <Heading margin="0px" fontSize="h4">переводим код между языками весело и задорно</Heading>
+          </FlexBox>
+          <img src={MainpageImage} className="r-stretch" alt=''/>
+        </FlexBox>
+      </Slide>
+      <Slide>
+        <FlexBox height="100%">
+          <Heading>Приветствие</Heading>
+          <Notes>
+            Всем привет! Меня зовут Олег. Сегодня я буду в легком обзорном стиле рассказывать про
+            компиляторы, транспиляторы и прочие инструменты для перевода кода с одного языка на другой.
+          </Notes>
+        </FlexBox>
+      </Slide>
+      <Slide>
+        <FlexBox height="100%" justifyContent='space-around'>
+          <img src={NstuImage} width="300px" alt='' />
+          <ul>
+            <li>АВТФ, 2010</li>
+            <li>Вычислительные комплексы системы и сети</li>
+            <li>Системная аналитика</li>
+          </ul>
+          <Notes>
+            Я выходец из НГТУ с факультета вычислительной техники, отучился на специальности "мастер на все руки",
+            умею в программирование, сети, микроконтроллеры и прочий computer science, полирнул это все магистратурой
+            по направлению системной аналитики, но на самом деле это не важно.
+          </Notes>
+        </FlexBox>
+      </Slide>
+      <Slide>
+        <FlexBox height="100%" justifyContent='space-around'>
+          <img src={GisImage} width="300px" alt='' />
+          <img src={WrikeImage} width="300px" alt='' />
+          <img src={VkImage} width="120px" alt='' />
+          <img src={FpjsImage} width="150px" alt='' />
+          <Notes>
+            Успел поработать в 2гис, wrike, Вконтакте, подрасти с середнячкового мидла до должности архитектора,
+            уйти обратно на мидла (правда в другую компанию) с сохранением зарплаты, ныне работаю так сказать в
+            стартапе внутри стартапа.
+          </Notes>
+        </FlexBox>
+      </Slide>
+    </Deck>
   );
 }
 
---
+++ src/theme/theme-default.js
@@ -0,0 +1,21 @@
+export default {
+    colors: {
+        primary: '#C4A287', // text, content
+        secondary: '#C4A287', // text, headers
+        tertiary: '#43281C', // background
+        quaternary: '#AB4E68',
+        quinary: '#B07156'
+    },
+    fonts: {
+        header: '"PT Sans Narrow", Helvetica, Arial, sans-serif',
+        text: '"PT Sans Narrow", Helvetica, Arial, sans-serif',
+    },
+    fontSizes: {
+        h1: '72px',
+        h2: '64px',
+        h3: '48px',
+        text: '24px',
+        monospace: '24px'
+    },
+    // space: number[];
+};
